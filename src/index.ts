518c47163435a1e72bec28ff4545120065380cd2
William Ravelo M <drummerwilliam@gmail.com>
2021-05-16 12:23:06 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { MobileProcess } from './mobile/MobileProcess';
 
 (async () => {
   let firstProcess: MobileProcess = new MobileProcess('93c6af52', 4740);
-  await firstProcess.start();
+  let firstClient = await firstProcess.start();
 
-  let secondProcess: MobileProcess = new MobileProcess('emulator-5554', 4728);
-  await secondProcess.start();
+  //let secondProcess: MobileProcess = new MobileProcess('emulator-5554', 4728);
+  //let secondClient = await secondProcess.start();
 
   await new Promise(r => setTimeout(r, 5000));
 
   firstProcess.stop();
-  secondProcess.stop();
+  //secondProcess.stop();
 })();
