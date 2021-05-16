0b15ea81daf27a98900567c602987b381103869f
William Ravelo M <drummerwilliam@gmail.com>
2021-05-15 22:44:44 -0500

    waiting for clients

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,14 @@
-const { remote } = require('webdriverio');
-const { exec } = require("child_process");
-class DeviceProcess {
-  deviceId: string;
-  port: number;
-  proc: any;
+import { MobileProcess } from './mobile/MobileProcess';
 
-  constructor(deviceId: string, port: number) {
-    this.deviceId = deviceId;
-    this.port = port;
-  }
+(async () => {
+  let firstProcess: MobileProcess = new MobileProcess('93c6af52', 4740);
+  await firstProcess.start();
 
-  async start() {
-    this.proc = exec(`appium -p ${this.port}`);
-    this.proc.stdout.on('data', this.onStdout.bind(this));
-    this.proc.stderr.on('data', this.onStderr.bind(this));
-  }
+  let secondProcess: MobileProcess = new MobileProcess('emulator-5554', 4728);
+  await secondProcess.start();
 
-  // Helpers
-  generaOpts(udid: string, port: number) {
-      return {
-        path: '/wd/hub',
-        port: port,
-        capabilities: {
-          platformName: "Android",
-          deviceName: "Android Emulator",
-          app: "./app.apk",
-          appPackage: "es.usc.citius.servando.calendula",
-          appActivity: "es.usc.citius.servando.calendula.activities.StartActivity",
-          automationName: "UiAutomator2",
-          udid: udid
-        }
-      }
-  };
+  await new Promise(r => setTimeout(r, 5000));
 
-  private async executeTests() {
-    console.log(`Starting process on device: ${this.deviceId}`);
-    const client = await remote(
-      this.generaOpts(
-        this.deviceId, this.port
-      )
-    );
-    await new Promise(r => setTimeout(r, 5000));
-    await client.deleteSession();
-    this.proc.kill('SIGINT');
-  }
-
-  private onStdout(data: any) {
-    let dataText: string = data.toString();
-    if(dataText.includes(`started on 0.0.0.0:${this.port}`)) {
-      this.executeTests();
-    }
-  }
-
-  private onStderr(data: any) {
-    let dataText: string = data.toString();
-    console.log(`Error starting process on device: ${this.deviceId}`);
-    console.log(dataText);
-    this.proc.kill('SIGINT');
-  }
-}
-
-let firstProcess: DeviceProcess = new DeviceProcess('93c6af52', 4723);
-firstProcess.start();
-
-let secondProcess: DeviceProcess = new DeviceProcess('emulator-5554', 4725);
-secondProcess.start();
+  firstProcess.stop();
+  secondProcess.stop();
+})();
---
+++ src/mobile/MobileProcess.ts
@@ -0,0 +1,84 @@
+const { remote } = require('webdriverio');
+const { exec } = require("child_process");
+
+export class MobileProcess {
+  deviceId: string;
+  port: number;
+  proc: any;
+  private client: any;
+  private defaultClientTimout: number;
+  private clientStartingTime: any;
+
+  constructor(deviceId: string, port: number) {
+    this.deviceId = deviceId;
+    this.port = port;
+    this.defaultClientTimout = 60000;
+  }
+
+  async start() {
+    this.proc = exec(`appium -p ${this.port}`);
+    this.proc.stdout.on('data', this.onStdout.bind(this));
+    this.proc.stderr.on('data', this.onStderr.bind(this));
+    this.clientStartingTime = Date.now();
+    return new Promise(this.waitForClientToStart.bind(this));
+  }
+
+  async stop() {
+    await this.client.deleteSession();
+    this.proc.kill('SIGINT');
+  }
+
+  // Helpers
+  generaOpts(udid: string, port: number) {
+      return {
+        path: '/wd/hub',
+        port: port,
+        capabilities: {
+          platformName: "Android",
+          deviceName: "Android Emulator",
+          app: "./app.apk",
+          appPackage: "es.usc.citius.servando.calendula",
+          appActivity: "es.usc.citius.servando.calendula.activities.StartActivity",
+          automationName: "UiAutomator2",
+          udid: udid
+        }
+      }
+  };
+
+  private async startProcess() {
+    console.log(`Starting process on device: ${this.deviceId}`);
+    this.client = await remote(
+      this.generaOpts(
+        this.deviceId, this.port
+      )
+    );
+  }
+
+  private waitForClientToStart(resolve: any, reject: any) {
+    if(this.client) {
+      resolve(this.client);
+    } else if(
+      (Date.now() - this.clientStartingTime) >= this.defaultClientTimout
+    ) {
+      reject(new Error("Timeout"));
+    } else {
+      setTimeout(
+        this.waitForClientToStart.bind(this, resolve, reject), 2000
+      );
+    }
+  }
+
+  private onStdout(data: any) {
+    let dataText: string = data.toString();
+    if(dataText.includes(`started on 0.0.0.0:${this.port}`)) {
+      this.startProcess();
+    }
+  }
+
+  private onStderr(data: any) {
+    let dataText: string = data.toString();
+    console.log(`Error starting process on device: ${this.deviceId}`);
+    console.log(dataText);
+    this.proc.kill('SIGINT');
+  }
+}
