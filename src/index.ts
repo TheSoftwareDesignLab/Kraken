d2792679eb7b44a3efe926f09afe4ec4d692decb
William Ravelo M <drummerwilliam@gmail.com>
2021-06-04 13:22:27 -0500

    web examples test

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,28 @@ import { WebClient } from './clients/WebClient';
 //  secondProcess.stop();
 //})();
 
-(async () => {
-    let firstProcess: WebClient = new WebClient('chrome');
-    let firstClient = await firstProcess.start();
-    await firstClient.url('https://www.google.com');
-    await new Promise(r => setTimeout(r, 5000));
-    firstProcess.stop();
-})();
+
+//const firstTest = async (client: any, process: any) => {    
+//    await client.url('https://www.google.com');
+//    await new Promise(r => setTimeout(r, 5000));
+//
+//    process.stop();
+//}
+
+//const secondTest = async (client: any, process: any) => {
+//    await client.url('https://www.google.com');
+//    await new Promise(r => setTimeout(r, 5000));
+//
+//    process.stop();
+//}
+
+//(async () => {
+//    let firstProcess: WebClient = new WebClient('chrome');
+//    let firstClient = await firstProcess.start();
+//
+//    let secondProcess: WebClient = new WebClient('chrome');
+//    let secondClient = await secondProcess.start();
+//
+//    firstTest(firstClient, firstProcess);
+//    secondTest(secondClient, secondProcess);
+//})();
