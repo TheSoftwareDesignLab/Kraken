4e65e7598bc4aad05f47ef09d617bc42b207c819
William Ravelo M <drummerwilliam@gmail.com>
2021-08-02 12:58:54 -0500

    web reporter

--- src/reports/Reporter.ts
+++ src/reports/Reporter.ts
@@ -4,9 +4,11 @@ import { WebDevice } from "../devices/WebDevice";
 import { TestScenario } from "../TestScenario";
 import * as Constants from '../utils/Constants';
 import { FileHelper } from '../utils/FileHelper';
+import ejs from 'ejs';
 
 export class Reporter {
     testScenario: TestScenario;
+    PASSED = 'passed';
 
     constructor(testScenario: TestScenario) {
         this.testScenario = testScenario;
@@ -73,6 +75,8 @@ export class Reporter {
 
     private generateEachDeviceReport() {
         this.testScenario.devices.forEach((device: Device, index: number) => {
+            if (!device) { return; }
+
             this.generateDeviceReport(device, index + 1);
         });
     }
@@ -88,14 +92,169 @@ export class Reporter {
     }
 
     private generateMobileReport(device: Device, userId: number) {
-
     }
 
-    private generateWebReport(device: Device, userId: number) {
-
+    private generateWebReport(device: Device, userId: number) {    
+        let cucumberFile = `${Constants.REPORT_PATH}/${this.testScenario.executionId}/${device.id}/${Constants.FILE_REPORT_NAME}`;
+        let features = JSON.parse(FileHelper.instance().contentOfFile(cucumberFile));
+        var data = {
+            apk_path: null,
+            features: features,
+            total_scenarios: this.totalScenariosForFeatures(features),
+            device: device, 
+            total_failed_scenarios_percentage: this.totalFailedScenariosPercentageForFeatures(features),
+            total_passed_scenarios_percentage: this.totalPassedScenariosPercentageForFeatures(features),
+            total_passed_features_percentage: this.totalPassedFeaturesPercentageForFeatures(features),
+            total_failed_features_percentage: this.totalFailedFeaturesPercentageForFeatures(features),
+            total_passed_features: this.totalPassedFeaturesForFeatures(features),
+            total_failed_features: this.totalFailedFeaturesForFeatures(features),
+            total_passed_scenarios: this.totalPassedScenariosForFeatures(features),
+            total_failed_scenarios: this.totalFailedScenariosForFeatures(features),
+            feature_passed: this.featurePassed.bind(this),
+            passed_scenarios: this.passedScenariosForFeature.bind(this),
+            failed_scenarios: this.failedScenariosForFeature.bind(this),
+            feature_duration: this.durationForFeature.bind(this),
+            format_duration: this.formatDurationOfFeature.bind(this)
+        }
+        let template = FileHelper.instance().contentOfFile(
+            `${__dirname}/../../reporter/feature_report.html.ejs`
+        )
+        let html = ejs.render(template, data);
+        let reportFilePath = `${Constants.REPORT_PATH}/${this.testScenario.executionId}/${device.id}/feature_report.html`;
+        FileHelper.instance().createFileIfDoesNotExist(reportFilePath);
+        FileHelper.instance().appendTextToFile(html, reportFilePath);
     }
 
     private generateGeneralReport() {
 
+    }
+
+    private totalScenariosForFeatures(features: any): number {
+        let count = 0;
+        features.forEach((feature: any) => {
+            let scenarios = feature.elements;
+            if(scenarios) {
+                count += scenarios.length;
+            }
+        });
+        return count;
+    }
+
+    private totalPassedScenariosPercentageForFeatures(features: any): number {
+        return parseFloat(
+            (this.totalPassedScenariosForFeatures(features) / this.totalScenariosForFeatures(features)).toFixed(2)
+        ) * 100.00;
+    }
+
+    private totalFailedScenariosPercentageForFeatures(features: any): number {
+        return parseFloat(
+            (this.totalFailedScenariosForFeatures(features) / this.totalScenariosForFeatures(features)).toFixed(2)
+        ) * 100.00;
+    }
+
+    private totalPassedFeaturesPercentageForFeatures(features: any): number {
+        return parseFloat(
+            (this.totalPassedFeaturesForFeatures(features) / features.length).toFixed(2)
+        ) * 100.00
+    }
+
+    private totalFailedFeaturesPercentageForFeatures(features: any): number {
+        return parseFloat(
+            (this.totalFailedFeaturesForFeatures(features) / features.length).toFixed(2)
+        ) * 100.00
+    }
+
+    private totalPassedScenariosForFeatures(features: any): number {
+        let count = 0;
+        features.forEach((feature: any) => {
+            count += this.passedScenariosForFeature(feature).length;
+        });
+        return count;
+    }
+
+    private totalFailedScenariosForFeatures(features: any): number {
+        let count = 0;
+        features.forEach((feature: any) => {
+            count += this.failedScenariosForFeature(feature).length;
+        });
+        return count;
+    }
+
+    private totalPassedFeaturesForFeatures(features: any): number {
+        let count = 0;
+        features.forEach((feature: any) => {
+            if (this.featurePassed(feature)) {
+                count += 1;
+            }
+        });
+        return count;
+    }
+
+    private totalFailedFeaturesForFeatures(features: any): number {
+        let count = 0;
+        features.forEach((feature: any) => {
+            if (!this.featurePassed(feature)) {
+                count += 1;
+            }
+        });
+        return count;
+    }
+
+    private featurePassed(feature: any): boolean {
+        return this.passedScenariosForFeature(feature).length == feature.elements.length;
+    }
+
+    private passedScenariosForFeature(feature: any): any {
+        let scenarios = feature.elements;
+        return scenarios.filter((scenario: any) => {
+            let steps = scenario.steps;
+            let allPassed = true;
+            steps.forEach((step: any) => {
+                if(step.result && step.result.status != 'passed') {
+                    allPassed = false;
+                }
+            });
+            return allPassed;
+        });
+    }
+
+    private failedScenariosForFeature(feature: any): any {
+        let scenarios = feature.elements;
+        return scenarios.filter((scenario: any) => {
+            let steps = scenario.steps;
+            let allPassed = true;
+            steps.forEach((step: any) => {
+                if (step.result && step.result.status != 'passed') {
+                    allPassed = false;
+                }
+            });
+            return !allPassed;
+        });
+    }
+
+    private durationForFeature(feature: any) {
+        let scenarios = feature.elements;
+        let duration = 0;
+        scenarios.forEach((scenario: any) => {
+            duration += this.durationForScenario(scenario);
+        });
+        return duration;
+    }
+
+    private durationForScenario(scenario: any) {
+        let duration = 0;
+        scenario.steps.forEach((step: any) => {
+            if(step.result && step.result.duration) {
+                duration += step.result.duration;
+            }
+        });
+        return duration;
+    }
+
+    private formatDurationOfFeature(nanoseconds: number): string {
+        let durationInSeconds = nanoseconds / 1000000000.0;
+        let mod = durationInSeconds % 60;
+        let div = Number(durationInSeconds / 60);
+        return `${div.toFixed(0)}m ${mod.toFixed(3)}s`;
     }
 }
